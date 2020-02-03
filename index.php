6127390fb75498224eb0e072de074b0ad7e297a2
Técnico em Informática 2019.6 <16121742019.6@senacrjedu.local>
2020-02-03 11:21:31 -0300

    css teste

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Projeto Integrador</title>
 </head>
 <body>
-    
+<h2>GAMES4YOU</h2>
+<hr> 
 </body>
 </html>
