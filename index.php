3423ef188950b3eecbb166b00560e8b6f55a1732
Técnico em Informática 2019.6 <16121742019.6@senacrjedu.local>
2020-02-03 11:30:24 -0300

    aprovado

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Projeto Integrador</title>
+    <link href="projeto.css" rel="stylesheet">
 </head>
 <body>
 <h2>GAMES4YOU</h2>
